1d3ab6dff5a928869d2fcb3d5481d07f6580a3cd
Hardik <hardik12498@gmail.com>
2017-07-03 03:58:02 +0530

    Initial commit

---
+++ wear/src/main/java/hardik124/silencio/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package hardik124.silencio;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("hardik124.silencio.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
