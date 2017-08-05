3a56270429a6c758a93f3b6e090e51c824d23312
Hardik <hardik12498@gmail.com>
2017-08-05 23:09:01 +0530

    Added set ringer mode service.

---
+++ mobile/src/main/java/hardik124/silencio/SetRingerMode.java
@@ -0,0 +1,40 @@
+package hardik124.silencio;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class SetRingerMode extends IntentService {
+
+    public SetRingerMode(String name) {
+        super(name);
+    }
+
+    public SetRingerMode() {
+        super("Set Ringer Mode");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+            Bundle extra = intent.getExtras();
+            if (extra == null)
+                return;
+            final int mode = extra.getInt("mode");
+            NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (android.os.Build.VERSION.SDK_INT < 24 ||
+                    (android.os.Build.VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted())) {
+                AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                audioManager.setRingerMode(mode);
+        }
+    }
+}
